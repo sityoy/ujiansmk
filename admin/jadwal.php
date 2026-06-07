0b5aca3cd1d3303d58668cc781b68cfcca4ae8a5
sityoy <tioirfanantoni@gmail.com>
2026-06-07 12:37:40 +0000

    oke

--- admin/jadwal.php
+++ admin/jadwal.php
@@ -1,7 +1,7 @@
 <?php
 // Tampilkan pesan error jika ada masalah agar kita tahu penyebabnya
 error_reporting(E_ALL);
-ini_set('display_errors', 1);
+// ini_set('display_errors', 1);
 
 require 'cek_admin.php';
 
