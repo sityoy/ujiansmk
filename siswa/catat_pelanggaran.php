21ceb91a2900ce62cf752fa51f10476ebdf7c123
sityoy <tioirfanantoni@gmail.com>
2026-06-07 10:52:09 +0000

    upadating

--- siswa/catat_pelanggaran.php
+++ siswa/catat_pelanggaran.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require 'cek_admin.php';
+require '../koneksi.php';
 
 if (isset($_POST['ujian_id'], $_POST['jumlah'], $_SESSION['ujian_id']) && (int) $_POST['ujian_id'] === (int) $_SESSION['ujian_id']) {
     $ujian_id = (int) $_POST['ujian_id'];
